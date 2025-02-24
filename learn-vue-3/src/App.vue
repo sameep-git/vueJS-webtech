21335db456d2da53b8f1f4b4f9e4d94e956d19d8
sameep-git <sameep.shah@tcu.edu>
2025-02-23 23:17:41 -0600

    first commit

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1> {{ message }} </h1>
+  <hr />
+  <h1>number: {{ number }}</h1>
+  <hr>
+  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
+  <hr>
+  <h1>nummber * 2: {{ number * 2 }}</h1>
+  <hr>
+  <h1>{{ number > 150? 'number is greater than 150': 'number is less than 150' }}</h1>
+  <hr>
+  <h1 v-text="number"></h1>
+  <hr>
+  <h1>{{ rawHtml }}</h1>
+  <hr>
+  <h1 v-text="rawHtml"></h1>
+  <hr>
+  <h1 v-html="rawHtml"></h1>
 </template>
 
 <script setup>
+let message = 'Hello, Vue!'
+let number = 50;
 
+function doubleNum(num) {
+  return num * 2;
+}
+
+let rawHtml = '<span style="color: red">This should be red.</span>'
 </script>
 
 <style lang="scss" scoped>
