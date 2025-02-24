5a9bab44bb19a076a47d839ac9b64def91a37a0c
sameep-git <sameep.shah@tcu.edu>
2025-02-23 23:59:32 -0600

    this is so much work... :(

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+  <img v-bind:src="imageUrl" alt="" />
+  <br>
+  <img :src="imageUrl" alt="" />
+
+  <button @click="changeImg">Change image</button>
+  <hr/>
+  <input type="text" :value="defaultInputText" />
+  <hr>
+  <p :class="className">Harry Potter</p>
+
+  <p :class="{ inactive: isInactive, center: isCenter }">
+    Harry Potter
+  </p>
+
+  <p :class="['active', 'center']">Harry Potter</p>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+let message = 'Hello, v-bind!';
 
+let imageUrl = ref('/public/img/banner_1.jpg');
+
+function changeImg() {
+  imageUrl.value = '/public/img/banner_2.jpg';
+}
+
+let defaultInputText = 'Write Something here...';
+
+let className = 'active';
+
+let isInactive = ref(false);
+let isCenter = ref(true);
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+img {
+  max-width: 300px;
+}
 
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
 </style>
