d3ee7067937e514bc6ddaf874203ea4946395554
sameep-git <sameep.shah@tcu.edu>
2025-03-08 11:17:11 -0600

    completed v-models

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <BlogPost v-for="post in posts" :key="post.id" v-bind="post" @delete-blog-post="processDeletion"></BlogPost>
+  <BlogPost v-for="post in posts" 
+  :key="post.id" :id="post.id"
+  v-model:blogPostTitle="post.blogPostTitle"
+  v-model:blogPostContent="post.blogPostContent" 
+  @delete-blog-post="processDeletion"></BlogPost>
 </template>
 
 <script setup>
