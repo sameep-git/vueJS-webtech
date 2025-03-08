df8c79f69923ce38b7ba2bf0e43e8087f7551dbd
sameep-git <sameep.shah@tcu.edu>
2025-03-08 11:43:54 -0600

    provide and inject

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>Message: {{ message }}</h1>
+  <div style="background-color: orange; padding-left: 10px;">
+    <ComponentA />
+  </div>
+  <button @click="count = count + 2">Add to count</button>
+  
 </template>
 
 <script setup>
+import { provide, readonly, ref } from 'vue';
+import ComponentA from './ComponentA.vue';
+
+let message = ref('Hello, Provide/inject!')
+
+let count = ref(0)
+
+function updateMessage() {
+  message.value = 'Hello, Provide/inject! Updated.'
+}
+
+provide('msg', {message, updateMessage})
+provide('read-only-count', readonly(count))
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
