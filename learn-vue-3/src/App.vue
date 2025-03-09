1e76cc3b452969b1625109fc43b03e608c661b2c
sameep-git <sameep.shah@tcu.edu>
2025-03-09 15:35:51 -0500

    finished slots intro

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>Hello, Vue!</div>
+  <AlertBox></AlertBox>
+  <AlertBox>Invalid Username or password. Please try again!</AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresh the page and try again.</h1>
+  </AlertBox>
 </template>
 
 <script setup>
+import AlertBox from './AlertBox.vue';
 
 </script>
 
